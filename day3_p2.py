2f8a44f4d1dbb6acb5cd01d2b708ca3bc92ddbb3
jimfarrell12 <60685521+jimfarrell12@users.noreply.github.com>
2024-12-05 14:16:45 -0600

    Create day3_p2.py

---
+++ day3_p2.py
@@ -0,0 +1,19 @@
+with file_path.open() as file:
+    data = file.read()
+
+pattern = re.compile(r"(do\(\))|(don't\(\))|mul\(([0-9]+),([0-9]+)\)")
+matches = pattern.findall(data)
+
+active = True
+ans = 0
+
+for do, dont, a, b in matches:
+    if do:
+        active = True
+    elif dont:
+        active = False
+    else:
+        product = int(a) * int(b)
+        ans += product * active
+
+print(ans)
