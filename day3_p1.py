c2291173ab17cb099283d4c98f3295ce63988ced
jimfarrell12 <60685521+jimfarrell12@users.noreply.github.com>
2024-12-05 12:30:37 -0600

    Update day3_p1.py

--- day3_p1.py
+++ day3_p1.py
@@ -8,8 +8,9 @@ matches = re.findall(pattern, data)
 
 ans = 0
 
-for pair in matches:
-    product = int(pair[0]) * int(pair[1])
+for match in matches:
+    nums = list(map(int, match))
+    product = nums[0] * nums[1]
     ans += product
 
 print(ans)
