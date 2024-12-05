6eb9a53be4007807d797749b38003812374cdafd
jimfarrell12 <60685521+jimfarrell12@users.noreply.github.com>
2024-12-05 17:51:04 -0600

    Update day2_p2.py

--- day2_p2.py
+++ day2_p2.py
@@ -1,13 +1,17 @@
-def pair_nums(nums):
+def is_safe(nums):
     pairs = list(zip(nums, nums[1:]))
-    return pairs
-
-def is_safe(pairs):
     increasing = all(a < b for a, b in pairs)
     decreasing = all(a > b for a, b in pairs)
     within_range = all(1 <= abs(a - b) <= 3 for a, b in pairs)
-    safety = (increasing or decreasing) and within_range
-    return safety
+    return (increasing or decreasing) and within_range
+
+def is_safe_dampener(nums):
+    for i in range(len(nums)):
+        alt_nums = nums[:]
+        alt_nums.pop(i)
+        if is_safe(alt_nums):
+            return True  
+    return False
 
 with open("input_day2.txt", "r") as file:
     lines = file.read().splitlines()
@@ -16,21 +20,7 @@ safe_count = 0
 
 for line in lines:
     nums = list(map(int, line.split()))
-    pairs = pair_nums(nums)
-    safe = is_safe(pairs)
-
-    if not safe:
-        for i in range(len(nums)):
-            alt_nums = nums[:]
-            alt_nums.pop(i)
-            alt_pairs = pair_nums(alt_nums)
-            alt_safe = is_safe(alt_pairs)
-
-            if alt_safe:
-                safe = True
-                break
-
-    if safe:
+    if is_safe(nums) or is_safe_dampener(nums):
         safe_count += 1
 
 print(safe_count)
